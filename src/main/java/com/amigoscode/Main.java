49e85154e3b60a837588297fac23ac2765cd7da9
Penny Pan <pennypan@Pennys-MacBook-Air.local>
2024-01-17 20:09:52 +0800

    first commit

---
+++ src/main/java/com/amigoscode/Main.java
@@ -0,0 +1,36 @@
+package com.amigoscode;
+
+import com.amigoscode.customer.Customer;
+import com.amigoscode.customer.CustomerController;
+import com.amigoscode.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+//        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
+//        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+//        for (String beanDefinitionName: beanDefinitionNames) {
+//            System.out.println(beanDefinitionName);
+//        }
+    }
+
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRespository) {
+        return args -> {
+            Customer alex = new Customer(1, "Alex", "alex@gmail.com", 21);
+            Customer jamila = new Customer(2, "Jamila", "jamila@gmail.com", 21);
+//            List<Customer> customers = List.of(alex, jamila);
+            customerRespository.saveAll(List.of(alex, jamila));
+        };
+    }
+}
---
+++ src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,36 @@
+package com.amigoscode.customer;
+
+import com.amigoscode.exception.ResourceNotFound;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+    private final CustomerDao customerDao;
+    public CustomerService(@Qualifier("jpa") CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    public List<Customer> getALlCustomers() {
+        return customerDao.selectAllCustomers();
+    }
+
+    public Optional getCustomer(Integer id) {
+        //        return customerDao.selectCustomerById(id)
+//                .orElseThrow(() ->  new ResourceNotFound("customer with id [%s] not found".formatted(id)));
+
+        Optional customer = customerDao.selectCustomerById(id);
+        if (customer.isPresent()) return customer;
+        throw new ResourceNotFound(
+                "customer with id [%s] not found".formatted(id));
+    }
+
+    public void addCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+        // check if email exists
+        // add
+
+    }
+}
